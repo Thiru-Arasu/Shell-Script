7b67173a2da5b4a7ef780af0dda250e8a2477352
Thirunavukkarasu <Thiru-Arasu@users.noreply.github.com>
2016-08-09 12:22:25 +0530

    Create expect_script.sh

---
+++ expect_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Ensure expect package is installed
+# Sample interaction mode script
+cat <<'EOF'> /tmp/hello-world
+#!/bin/bash
+echo "Enter the word: "
+read word
+echo "Result: $word"
+EOF
+chmod 755 /tmp/hello-world
+
+# Expect command
+cat <<'EOF'> /tmp/expect.sh
+expect -c "
+set 1 [lindex $argv 0]
+
+# Actual command
+spawn /tmp/hello-world
+expect \"Enter the word: \"
+send \"$1\n\";
+
+expect eof "
+EOF
+
+/bin/bash /tmp/expect.sh "Your text" 
+rm /tmp/expect.sh /tmp/hello-world
